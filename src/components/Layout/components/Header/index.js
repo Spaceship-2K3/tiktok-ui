decae09c78b9c9f67d0733595fe953b5ff485ff3
DV.Sang <vansangIT2k3@gmai.com>
2023-12-13 22:12:48 +0700

    heaader ui #5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -25,6 +25,29 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faLanguage} />,
         title: "English",
+        children: {
+            title: "Language",
+            data: [
+                {
+                    type: "Language",
+                    code: "en",
+                    title: "English",
+                    /*children: {
+                        title: "Language",
+                        data: [
+                            {
+                                code: "en",
+                                title: "English1",
+                            },
+                            { code: "vi", title: "Vietnamese1" },
+                            { code: "fr", title: "French1" },
+                        ],
+                    }*/
+                },
+                { type: "Language", code: "vi", title: "Vietnamese" },
+                { type: "Language", code: "fr", title: "French" },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -44,6 +67,16 @@ function Header() {
             setSearchResult([]);
         }, 0);
     }, []);
+
+    // handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case "language":
+                // Handle change language
+                break;
+            default:
+        }
+    };
 
     return (
         <header className={cx("wrapper")}>
@@ -98,7 +131,7 @@ function Header() {
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
 
-                    <Menu items={MENU_ITEMS}>
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                         <button className={cx("more-btn")}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -4,13 +4,33 @@ import React, { useEffect, useState, forwardRef, Children } from "react";
 import classNames from "classnames/bind";
 import styles from "./Menu.module.scss";
 import Tippy from "@tippyjs/react/headless";
+import Header from "./Header";
 
 const cx = classNames.bind(styles);
+const defaultFn = () => {};
 
-function Menu({ children, items = [] }) {
+function Menu({ children, items = [], onChange = defaultFn }) {
+    // Lan dau tien render chi ra trang nhat, obj dai dien cho du lieu trang hien tai
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+
     const renderItem = () => {
-        return items.map((item, index) => {
-            return <MenuItem key={index} data={item} />;
+        return current.data.map((item, index) => {
+            // Khi click vao thang nao la cha thi se chuyen sang menu cap 2 cua no
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
         });
     };
     return (
@@ -21,6 +41,17 @@ function Menu({ children, items = [] }) {
             render={(attrs) => (
                 <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
                     <PopperWrapper className={cx("menu-popper")}>
+                        {/*   xu li khi mang k phai la phan tu dau tien thi xoa phan tu thu 2 roi back ve */}
+                        {history.length > 1 && (
+                            <Header
+                                title="Language"
+                                onBack={() => {
+                                    setHistory((prev) =>
+                                        prev.slice(0, prev.length - 1)
+                                    );
+                                }}
+                            />
+                        )}
                         {renderItem()}
                     </PopperWrapper>
                 </div>
